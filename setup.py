5109dbe43f59b4c3f64956645e7e3582ca2bdfca
Akshat <akshatbansal217@gmail.com>
2025-11-17 20:18:32 +0530

    project structure ready, venev set

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    
+    name = "src",
+    version = "0.0.1",
+    author = "Akshat Bansal",
+    author_email = "akshatbansal217@gmail.com",
+    packages=find_packages()
+)
